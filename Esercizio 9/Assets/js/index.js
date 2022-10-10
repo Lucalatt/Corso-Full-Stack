09faa0f5db572ddcb78c97366df8632596b04c3b
Lucalatt <113041623+Lucalatt@users.noreply.github.com>
2022-10-10 18:26:02 +0300

    Add files via upload
    Esercizio 9 10/10

---
+++ Esercizio 9/Assets/js/index.js
@@ -0,0 +1,3 @@
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+alert('Stiamo parlando del più e del meno');
---
+++ Esercizio 9/Assets/js/naming.js
@@ -0,0 +1,30 @@
+
+document .getElementById("boh").innerHTML="cacao"
+
+
+var pet= 'gatto';
+console.log(pet);
+
+var pet = 'gatto';
+console.log(pet);
+var Pet = 'cane';
+console.log (Pet);
+var petPreferito = 'criceto';
+console.log (petPreferito);
+var PetPreferito = 'coniglio';
+console.log (PetPreferito);
+
+var $pet = 'giraffa';
+console.log ($pet);
+var _pet = 'leone';
+console.log (_pet);
+console.log ($pet + _pet)
+
+
+var dodici= '12';
+console.log (dodici-10);
+
+
+
+var _pet2 = 'pantera';
+document.write(_pet2);
